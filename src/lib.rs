3194489ac160d660411fdb9237f89dcec3cd0496
Barry Moore <chiroptical@gmail.com>
2022-06-23 08:57:18 -0400

    Use slice_patterns to implement head

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
 pub mod chapters;
 
+// The `Copy` trait allows you to copy values that would otherwise be moved. In this case, we want
+// a new `T` not a reference to one (for whatever reason)
+#[allow(dead_code)]
+fn head<T: Copy>(xs: &[T]) -> Option<T> {
+    match &xs {
+        [x, ..] => Some(*x),
+        _ => None,
+    }
+}
+
 pub fn main() {
     println!("Introduction to Algorithms!");
 }
