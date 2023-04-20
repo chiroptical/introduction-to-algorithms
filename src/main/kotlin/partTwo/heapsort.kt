3ed51babca756e4222ba86162970b43abae49ebf
chiroptical <chiroptical@gmail.com>
2023-04-20 18:08:19 -0400

    Fix indices, add maxHeapify

--- src/main/kotlin/partTwo/heapsort.kt
+++ src/main/kotlin/partTwo/heapsort.kt
@@ -2,9 +2,9 @@ package partTwo
 
 fun parentIndex(n: UInt): UInt = n / 2u
 
-fun leftSubtreeIndex(n: UInt): UInt = 2u * n
+fun leftSubtreeIndex(n: UInt): UInt = 2u * n + 1u
 
-fun rightSubtreeIndex(n: UInt): UInt = 2u * n + 1u
+fun rightSubtreeIndex(n: UInt): UInt = 2u * n + 2u
 
 // max-heap sort ensures, for every non-root node, a[parent(i)] > a[i]
 
@@ -28,3 +28,33 @@ fun maxElementsBy(height: UInt): UInt =
         0u, 1u -> height
         else -> height * height - 1u
     }
+
+fun <T> MutableList<T>.swap(from: Int, to: Int) {
+    val fromTemp = this.getOrNull(from)
+    val toTemp = this.getOrNull(to)
+    if (fromTemp != null && toTemp != null) {
+        this[from] = toTemp
+        this[to] = fromTemp
+    }
+}
+
+// TODO Exercise 6.2-3: write minHeapify
+// TODO Exercise 6.2-4: what happens when input[index] > than its' children?
+// TODO Exercise 6.2-5: what happens when index > input.size / 2?
+// TODO Exercise 6.2-6: is it possible to convert the recursive call to a loop?
+fun <T : Comparable<T>> maxHeapify(input: MutableList<T>, index: UInt) {
+    val heapSize = input.size.toUInt()
+    val left = leftSubtreeIndex(index)
+    val right = rightSubtreeIndex(index)
+    var largest: UInt = index
+    if (left < heapSize && input[left.toInt()] > input[index.toInt()]) {
+        largest = left
+    }
+    if (right < heapSize && input[right.toInt()] > input[largest.toInt()]) {
+        largest = right
+    }
+    if (largest != index) {
+        input.swap(index.toInt(), largest.toInt())
+        maxHeapify(input, largest)
+    }
+}
--- src/test/kotlin/partTwo/heapsortTest.kt
+++ src/test/kotlin/partTwo/heapsortTest.kt
@@ -3,7 +3,57 @@ package partTwo
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-infix fun <T> T.isEqualTo(got: T) = assertEquals(this, got)
+infix fun <T> T.isEqualTo(expected: T) = assertEquals(expected, this)
+
+internal class Indices {
+    @Test
+    fun `can get parent index for zero`() {
+        parentIndex(0u) isEqualTo 0u
+    }
+
+    @Test
+    fun `can get parent index for one`() {
+        parentIndex(1u) isEqualTo 0u
+    }
+
+    @Test
+    fun `can get parent index for three`() {
+        parentIndex(3u) isEqualTo 1u
+    }
+
+    @Test
+    fun `can get parent index for five`() {
+        parentIndex(5u) isEqualTo 2u
+    }
+
+    @Test
+    fun `can get subtree indices for zero`() {
+        val idx = 0u
+        leftSubtreeIndex(idx) isEqualTo 1u
+        rightSubtreeIndex(idx) isEqualTo 2u
+    }
+
+    @Test
+    fun `can get subtree indices for one`() {
+        val idx = 1u
+        leftSubtreeIndex(idx) isEqualTo 3u
+        rightSubtreeIndex(idx) isEqualTo 4u
+    }
+
+    @Test
+    fun `can get subtree indices for two`() {
+        val idx = 2u
+        leftSubtreeIndex(idx) isEqualTo 5u
+        rightSubtreeIndex(idx) isEqualTo 6u
+    }
+
+    @Test
+    fun `can get subtree indices for three`() {
+        val idx = 3u
+        leftSubtreeIndex(idx) isEqualTo 7u
+        rightSubtreeIndex(idx) isEqualTo 8u
+    }
+}
 
 internal class MinElementsBy {
 
@@ -57,5 +107,21 @@ internal class MaxElementsBy {
     @Test
     fun `it handles 4`() {
         maxElementsBy(4u) isEqualTo 15u
+    }
+}
+
+internal class MaxHeapify {
+    @Test
+    fun `figure 6_2 test`() {
+        val heap = mutableListOf<Int>(16, 4, 10, 14, 7, 9, 3, 2, 8, 1)
+        maxHeapify(heap, 1u)
+        heap isEqualTo mutableListOf<Int>(16, 14, 10, 8, 7, 9, 3, 2, 4, 1)
+    }
+
+    @Test
+    fun `exercise 6_2-1 test`() {
+        val heap = mutableListOf<Int>(27, 17, 3, 16, 13, 10, 1, 5, 7, 12, 4, 8, 9, 0)
+        maxHeapify(heap, 2u)
+        heap isEqualTo mutableListOf<Int>(27, 17, 10, 16, 13, 9, 1, 5, 7, 12, 4, 8, 3, 0)
     }
 }
